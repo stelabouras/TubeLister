caabe8f912213591b5b69943fff9e13292f87057
Stelios Petrakis <stelios@evilwindowdog.com>
2017-03-06 11:59:22 +0200

    TubeLister 1.6
    * TubeLister now places the currently playing video in a playlist as the first in the new playlist it will create
    * If a YouTube playlist in an active tab its videos will be placed first in the newly created playlist

--- js/events.js
+++ js/events.js
@@ -2,136 +2,134 @@ chrome.browserAction.onClicked.addListener(() => {
 
   chrome.windows.getCurrent((window) => {
 
-    var currentWindowId = window.id;
     var maxPlaylistVideos = 50;
+    var currentWindowId   = window.id;
 
     chrome.tabs.query(
       {}, 
       (tabs) => {
 
-        if(!tabs)
+      if(!tabs)
+        return;
+
+      var videoIds = [];
+      var tabIdsToClose = [];
+      var fulfilledTabs = [];
+      var activeTabVideoIds = [];
+
+      var isYoutubeVideoId = (videoId) => { return videoId.match(/^[A-Za-z0-9_-]{11}$/); };
+
+      var compressTabs = () => {
+
+        if(fulfilledTabs.length > 0)
           return;
 
-        var videoIds = [];
-        var tabIdsToClose = [];
-        var fulfilledPlaylists = [];
-        var activeTabVideoId = null;
+        if(activeTabVideoIds.length > 0) {
 
-        var isYoutubeVideoId = (videoId) => { return videoId.match(/^[A-Za-z0-9_-]{11}$/); }
-        var compressTabs = () => {
+          for(var i = activeTabVideoIds.length - 1; i >= 0; i--) {
 
-          if(fulfilledPlaylists.length > 0)
+            var videoId = activeTabVideoIds[i];
+
+            if(videoIds.indexOf(videoId) == -1)
+              videoIds.unshift(videoId);
+          }
+        }
+
+        if(tabIdsToClose.length <= 1)
+          return;
+
+        tabIdsToClose.forEach((tabId) => {  chrome.tabs.remove(tabId); });
+
+        var url = 'https://www.youtube.com/watch_videos?video_ids=' + videoIds.join(',');
+
+        chrome.tabs.create({ 'url' : url });     
+      };
+    
+      tabs.forEach((tab) => {
+
+        if(tab.url.startsWith('view-source:'))
+          return;
+
+        var urlParts = tab.url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/|\&|\#)/);
+
+        if(urlParts == null)
+          return;
+
+        if(urlParts.length < 3)
+          return;
+
+        // Check if we are on the YouTube domain
+        var isYoutubeDomain = false;
+
+        if(urlParts[0].includes('youtube.com') || urlParts[0].includes('youtu.be'))
+          isYoutubeDomain = true;
+        else if(urlParts[1].includes('youtube.com') || urlParts[1].includes('youtu.be'))
+          isYoutubeDomain = true;
+
+        if(!isYoutubeDomain)
+          return;
+
+        var isActiveTab = (tab.active && tab.windowId == currentWindowId);
+
+        fulfilledTabs.push(tab.id);
+
+        chrome.tabs.executeScript(tab.id, {
+          file: "js/extractor.js",
+          runAt: "document_end"
+        }, (result) => {
+
+          fulfilledTabs.splice(fulfilledTabs.indexOf(tab.id), 1);
+
+          if(result.length == 0)
             return;
 
-          if(activeTabVideoId && videoIds.indexOf(activeTabVideoId) == -1)
-            videoIds.unshift(activeTabVideoId);
-
-          if(tabIdsToClose.length <= 1)
+          if(result[0].length == 0)
             return;
 
-          tabIdsToClose.forEach((tabId) => { chrome.tabs.remove(tabId); });
+          var playlistVideos = result[0];
 
-          var url = 'http://www.youtube.com/watch_videos?video_ids=' + videoIds.join(',');
+          var videosToAdd = [];
 
-          chrome.tabs.create({ 'url' : url });     
-        };
-      
-        tabs.forEach((tab) => {
+          // Check for duplicates
+          playlistVideos.forEach((videoId) => {
 
-          if(tab.url.startsWith('view-source:'))
-            return;
+            if(!isYoutubeVideoId(videoId))
+              return;
 
-          var urlParts = tab.url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/|\&|\#)/);
+            if(videoIds.indexOf(videoId) == -1)
+              videosToAdd.push(videoId);
+          });
 
-          if(urlParts == null)
-            return;
+          if(videosToAdd.length == maxPlaylistVideos) {
 
-          if(urlParts.length < 3)
-            return;
-
-          // Check if we are on the YouTube domain
-          var isYoutubeDomain = false;
-
-          if(urlParts[0].includes('youtube.com') || urlParts[0].includes('youtu.be'))
-            isYoutubeDomain = true;
-          else if(urlParts[1].includes('youtube.com') || urlParts[1].includes('youtu.be'))
-            isYoutubeDomain = true;
-
-          if(!isYoutubeDomain)
-            return;
-
-          // Extract any videos from playlists
-          if(tab.url.includes('list=')) {
-
-            fulfilledPlaylists.push(tab.id);
-
-            chrome.tabs.executeScript(tab.id, {
-              file: "js/extractor.js",
-              runAt: "document_end"
-            }, (result) => {
-
-              fulfilledPlaylists.splice(fulfilledPlaylists.indexOf(tab.id), 1);
-
-              if(result.length == 1 && result[0].length > 0) {
-
-                var playlistVideos = result[0];
-
-                var videosToAdd = [];
-
-                // Check for duplicates
-                playlistVideos.forEach((videoId) => {
-
-                  if(!isYoutubeVideoId(videoId))
-                    return;
-
-                  if(videoIds.indexOf(videoId) == -1)
-                    videosToAdd.push(videoId);
-                });
-
-                if(videosToAdd.length == maxPlaylistVideos) {
-
-                  compressTabs();
-                  return;
-                }
-
-                if(videosToAdd.length + videoIds.length > maxPlaylistVideos) {
-
-                  compressTabs();
-                  return;
-                }
-
-                tabIdsToClose.push(tab.id);
-
-                videosToAdd.forEach((videoId) => { videoIds.push(videoId); });
-              }
-        
-              compressTabs();
-            });
-
+            compressTabs();
             return;
           }
 
-          var videoId = urlParts[2];
+          if(videosToAdd.length + videoIds.length > maxPlaylistVideos) {
 
-          if(!isYoutubeVideoId(videoId))
+            compressTabs();
             return;
-
-          // Check for duplicates
-          if(videoIds.indexOf(videoId) != -1)
-            return;
-
-          if(videoIds.length + 1 > maxPlaylistVideos)
-            return;
-
-          if(tab.active && tab.windowId == currentWindowId)
-            activeTabVideoId = videoId;
-          else
-            videoIds.push(videoId);
+          }
 
           tabIdsToClose.push(tab.id);
+
+          videosToAdd.forEach((videoId) => { 
+
+            if(isActiveTab)
+              activeTabVideoIds.push(videoId);
+            else
+              videoIds.push(videoId); 
+          });
+    
+          compressTabs();
+
         });
 
-        compressTabs();           
+      });
+         
     });
+
   });
+
 });
--- js/extractor.js
+++ js/extractor.js
@@ -8,23 +8,38 @@ if(!window.TubeListerExtractor) {
 
             var playlistElement = document.querySelector('[data-list-title]');
 
-            // Make sure we are only extracting videos from
-            // the 'Untitled list' playlists
-            if(playlistElement.dataset.listTitle.localeCompare('Untitled list') != 0)
-                return [];
-
-            // Make sure the playlist is not shared
-            if(playlistElement.dataset.shareable.localeCompare('False') != 0)
-                return [];
-
             var videoIds = [];
+            
+            if(playlistElement) {
 
-            document.querySelectorAll('#playlist-autoscroll-list li').forEach(function(element) { 
-                videoIds.push(element.dataset.videoId);
-            });
+                if(
+                    // Make sure we are only extracting videos from
+                    // the 'Untitled list' playlists
+                    playlistElement.dataset.listTitle.localeCompare('Untitled list') == 0 &&
+                    // Make sure the playlist is not shared
+                    playlistElement.dataset.shareable.localeCompare('False') == 0 ) {
+
+                    var currentlyPlaying = null;
+
+                    document.querySelectorAll('#playlist-autoscroll-list li').forEach(function(element) { 
+
+                        if(element.className.indexOf('currently-playing') > -1)
+                            currentlyPlaying = element.dataset.videoId;
+                        else
+                            videoIds.push(element.dataset.videoId);
+                    });
+
+                    // Get the currently playing video first
+                    // on the list
+                    if(currentlyPlaying)
+                        videoIds.unshift(currentlyPlaying);
+                }
+            }
+            else
+                videoIds.push(document.querySelector('[data-video-id]').dataset.videoId);
 
             return videoIds;
-        },
+        }
       };
 
       window.TubeListerExtractor = TubeListerExtractor;
